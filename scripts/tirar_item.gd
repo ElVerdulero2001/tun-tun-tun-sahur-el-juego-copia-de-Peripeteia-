fca963facb8ef05e51e3fbd0b62cfaf155b922fd
ElVerdulero2001 <ElVerdulero2001@proton.me>
2026-04-19 18:29:46 -0300

    sistema de lanzamiento con velocidad angular por objeto

--- scripts/tirar_item.gd
+++ scripts/tirar_item.gd
@@ -6,22 +6,14 @@ func _input(event):
 	if event.is_action_pressed("tirar_item"):
 		if Inventario.items.size() > 0:
 			var player = get_tree().get_first_node_in_group("player")
-			if player == null or not player.is_inside_tree():
-				return
 			var camara = player.get_node("Camera3D")
-			if camara == null or not camara.is_inside_tree():
-				return
 			var instancia = Inventario.items[-1]
 			var posicion = camara.global_position + (-camara.global_transform.basis.z * 1.5)
 			var escena = Catalogo.get_prefab(instancia["data"].item_id)
-			if escena == null:
-				return
 			var nodo = escena.instantiate()
 			get_tree().current_scene.add_child(nodo)
 			nodo.global_position = posicion
-			var impulso = -camara.global_transform.basis.z * 5.0 + Vector3(0, 2, 0)
-			nodo.apply_impulse(impulso)
+			nodo.global_transform.basis = camara.global_transform.basis
+			nodo.apply_impulse(-camara.global_transform.basis.z * instancia["data"].fuerza_lanzamiento + Vector3(0, 2, 0))
+			nodo.angular_velocity = camara.global_transform.basis.x * instancia["data"].velocidad_angular
 			Inventario.items.erase(instancia)
-			print("Item tirado: ", instancia["data"].nombre)
-		else:
-			print("Inventario vacio")
